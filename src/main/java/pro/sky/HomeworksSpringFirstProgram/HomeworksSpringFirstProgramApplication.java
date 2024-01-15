f28ef20468adfd8ee0ab129279fde75ab5a0ac38
Nikolay <koliamba_5@mail.ru>
2024-01-15 20:18:02 +0800

    Initial

---
+++ src/main/java/pro/sky/HomeworksSpringFirstProgram/HomeworksSpringFirstProgramApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HomeworksSpringFirstProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworksSpringFirstProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworksSpringFirstProgramApplication.class, args);
+	}
+
+}
